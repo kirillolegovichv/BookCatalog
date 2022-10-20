793ea48a250b15febbbd2d139694a1a267d0c58a
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-20 20:22:20 +0300

    Add class DTO for Author

---
+++ BookCatalog.M/DTO/AuthorDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BookCatalog.M;
+
+public class AuthorDTO
+{
+    public int Id { get; set; }
+
+    public string Surname { get; set; }
+
+    public string Name { get; set; }
+
+    public string Patronimic { get; set; }
+}
--- BookCatalog.M/DTO/BookDTO.cs
+++ BookCatalog.M/DTO/BookDTO.cs
@@ -4,6 +4,8 @@ namespace BookCatalog.M;
 
 public class BookDTO
 {
+    public int Id { get; set; }
+
     public string Title { get; set; }
 
     public string Auther { get; set; }
