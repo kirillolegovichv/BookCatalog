adbdbffaad0388a5fd578f8a9809a6165046106c
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-20 20:39:26 +0300

    Add ServerOption

---
+++ BookCatalog.M/ServerOption.cs
@@ -0,0 +1,7 @@
+﻿namespace BookCatalog.M;
+
+public class ServerOption
+{
+    private const string _KirillServer = @"Server=DESKTOP-0HKPMGU;Database=;Trusted_Connection=True;";
+    public const string ConnectionOption = _KirillServer;
+}
---
+++ BookCatalog.M/StoredProcedure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalog.M
+{
+    internal class StoredProcedure
+    {
+    }
+}
