b5e04f6786fd366cab75497a947594df3782a6ae
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-24 17:54:08 +0300

    Add DataStorage method

---
+++ BookCatalog.VM/DataStorage.cs
@@ -0,0 +1,17 @@
+﻿using BookCatalog.VM.Models;
+using BookCatalog.M;
+
+namespace BookCatalog.VM;
+
+public class DataStorage
+{
+    public List<BookModel> Books { get; set; }
+    public List<AuthorModel> Authors { get; set; }
+
+    private static DataStorage _instance;
+
+    private BookManager bookManager = new BookManager();
+    private AuthorManager authorManager = new AuthorManager();
+
+
+}
