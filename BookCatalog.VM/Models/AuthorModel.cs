0ce60b519713c50f818738adcad671e8598ff111
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-24 17:34:44 +0300

    Add Author and Book Models

---
+++ BookCatalog.VM/Models/AuthorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BookCatalog.VM.Models;
+
+public class AuthorModel
+{
+    public int Id { get; set; }
+
+    public string Surname { get; set; }
+
+    public string Name { get; set; }
+
+    public string Patronimic { get; set; }
+
+    public AuthorModel()
+    {
+
+    }
+}
---
+++ BookCatalog.VM/Models/BookModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace BookCatalog.VM.Models;
+
+public class BookModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public int AuthorId { get; set; }
+
+    public int YearOfPublishing { get; set; }
+
+    public int ISBN { get; set; }
+
+    public string Description { get; set; }
+
+    public Bitmap Image { get; set; }
+
+    public BookModel()
+    {
+
+    }
+}
